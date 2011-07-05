2851286aa65456f191c66769969f575dbb392355
George Li <floogcrap@gmail.com>
2011-07-05 14:30:14 -0700

    Excercises

---
+++ ch1/02.c
@@ -0,0 +1,16 @@
+/* experiment with some escape sequences*/
+#include <stdio.h>
+
+main()
+ {
+	printf("hello, world\n");
+	printf("\a hello, world\n");
+	printf("hello, \b world\n");
+	printf("hello, \f world\n");
+	printf("hello, \r world\n");
+	printf("hello, \t world\n");
+	printf("hello, world\v\n");
+	printf("hello, world\?\n");
+	printf("\'hello\', world\n");
+ }
+	
