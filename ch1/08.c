5b78ebfbf6c8e87a88559ce8cc1226ca46aec67f
George Li <floogcrap@gmail.com>
2011-07-07 12:07:41 -0700

    Now counts new lines, tabs and blank spaces separately.

--- ch1/08.c
+++ ch1/08.c
@@ -3,16 +3,19 @@
    /* count blanks, tabs and newlines */ 
 main()
 {
-	int c, nl;
+	int c, nl, nt, nb;
 	
 	nl = 0;
+	nt = 0;
+	nb = 0;
+	
 	while ((c = getchar()) != EOF) {
 		if (c == '\n')
 			++nl;
 		if (c == '\t')
-			++nl;
+			++nt;
 		if (c == ' ')
-			++nl;
+			++nb;
 		}
-		printf("%d\n", nl);
+		printf("There are %d new line(s), %d tab(s), and %d blank space(s).\n", nl, nt, nb);
 }
