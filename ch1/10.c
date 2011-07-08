4b21e025202f60a87d5a2f017b49255e69892730
George Li <floogcrap@gmail.com>
2011-07-07 23:21:23 -0700

    Excercise 1-10

---
+++ ch1/10.c
@@ -0,0 +1,30 @@
+ #include <stdio.h> 
+ 
+   /* Copy input to output, replace each tab by \t, each backspace by \b, and each backslash by \\ */ 
+main()
+{
+	double c, d;
+	
+
+
+	
+	for (d = 0; (c = getchar()) != EOF; d = 0) {
+		if (c == '\b'){
+			putchar('\\');
+			putchar('b');
+			d = 1;
+			}
+		if (c == '\t'){
+			putchar('\\');
+			putchar('t');
+			d = 1;
+			}
+		if (c == '\\'){
+			putchar('\\');
+			putchar('\\');
+			d = 1;
+			}
+		if (d == '0')
+			putchar('c');
+		}
+}
