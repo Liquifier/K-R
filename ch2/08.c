71b011ddbb623e89f0d6815118d9fa6a437be233
George Li <floogcrap@gmail.com>
2011-08-01 18:51:30 -0700

    Wat is works just fine if you set x to unsigned int in the function

--- ch2/08.c
+++ ch2/08.c
@@ -3,10 +3,21 @@
  */
  
 #include <stdio.h>
-#define ARCHIT 32 /* Assume unsigned int has 32 bits */
+#define BITS 32 /* assume unsigned int is 32 bits*/
 
 unsigned int rightrot(unsigned int x, int n)
 {
-	return (x >> n) | (x << (ARCHIT - n));
+	
+	return (x >> n) | (x << (BITS - n));
 }
 
+int main()
+{
+	unsigned int x = 2;
+	int n = 1;
+	
+	printf("Rotate %u to the right by %d place(s) is %u\n", x, n, rightrot(x,n));
+	
+	return 0;
+	
+}
